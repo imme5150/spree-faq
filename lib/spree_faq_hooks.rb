7690a95e5d8da8a57b428ee19b60f3e59c70ab5f
Fabien Franzen <info@atelierfabien.be>
2011-07-28 13:40:39 +0200

    Fixed hooks deprecation using Deface

--- lib/spree_faq_hooks.rb
+++ lib/spree_faq_hooks.rb
@@ -1,6 +1,3 @@
-class SpreeFaqHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_tabs do
-    %(<%=  tab(:question_categories, :label => :question_categories_admin)  %>)
-  end
-end
-
+Deface::Override.new(:virtual_path => "layouts/admin",
+                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :text =>  %[<%=  tab(:question_categories, :label => :question_categories_admin)  %>])
