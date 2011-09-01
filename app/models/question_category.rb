1391cf224ee588d223b9fff3707c894b09944975
Fabien Franzen <info@atelierfabien.be>
2011-09-01 19:13:54 +0200

    Added locale attribute to QuestionCategory - fixed bugs/compatibility

--- app/models/question_category.rb
+++ app/models/question_category.rb
@@ -6,4 +6,7 @@ class QuestionCategory < ActiveRecord::Base
   validates_presence_of :name
 
   accepts_nested_attributes_for :questions, :allow_destroy => true
+  
+  scope :locale, lambda { |locale| where(:locale => locale) }
+  
 end
