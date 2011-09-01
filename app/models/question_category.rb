55e596e9d78264db4d9756b851754e79b3ccb291
Fabien Franzen <info@atelierfabien.be>
2011-09-01 19:28:48 +0200

    Revert "Backported changes/fixes from i18n branch"
    This reverts commit 60e9334828a248cdc332fb5de5d0b2a0ad2bd843.

--- app/models/question_category.rb
+++ app/models/question_category.rb
@@ -7,4 +7,6 @@ class QuestionCategory < ActiveRecord::Base
 
   accepts_nested_attributes_for :questions, :allow_destroy => true
   
+  scope :locale, lambda { |locale| where(:locale => locale) }
+  
 end
