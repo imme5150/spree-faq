a808430642c5b1848d9ec35a7a6d0c90b9ac118d
Fabien Franzen <info@atelierfabien.be>
2011-10-17 12:02:41 +0200

    Added :name to hooks

--- lib/spree_faq_hooks.rb
+++ lib/spree_faq_hooks.rb
@@ -1,3 +1,4 @@
 Deface::Override.new(:virtual_path => "layouts/admin",
+                     :name => 'faq_admin_tabs',
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text =>  %[<%=  tab(:question_categories, :label => :question_categories_admin)  %>])
