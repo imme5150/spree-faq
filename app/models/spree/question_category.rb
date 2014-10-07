fab33ae4b13964a9357b8405cd95fd3eacce6bf5
Josh Shupack <ynaught@gmail.com>
2014-10-07 09:52:42 -0700

    removed locale

--- app/models/spree/question_category.rb
+++ app/models/spree/question_category.rb
@@ -2,16 +2,14 @@ module Spree
   class QuestionCategory < ActiveRecord::Base
     acts_as_list
     default_scope :order => "position ASC"
-    
-    attr_accessible :name, :locale, :questions_attributes
+
+    attr_accessible :name, :questions_attributes
 
     has_many :questions
-    validates_uniqueness_of :name, :scope => :locale
+    validates_uniqueness_of :name
     validates_presence_of :name
 
     accepts_nested_attributes_for :questions, :allow_destroy => true
-  
-    scope :locale, lambda { |locale| where(:locale => locale) }
-  
+
   end
 end
